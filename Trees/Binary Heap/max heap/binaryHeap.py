0b03656b97bef81f5f5271e8874ab0e92e51f496
recep-yildirim <0.recepyildirim@gmail.com>
2020-12-16 10:02:30 +0300

    Binary Heaps were added.

---
+++ Trees/Binary Heap/max heap/binaryHeap.py
@@ -0,0 +1,56 @@
+class BinaryHeap () :
+    def __init__ (self) :
+        self.heap_list = [0]
+        self.current_size = 0
+
+    def percUp (self , i) :
+        while i // 2 > 0 :
+            if self.heap_list [i] > self.heap_list [i // 2] :
+                self.heap_list [i] , self.heap_list [i // 2] = self.heap_list [i // 2] , self.heap_list [i]
+
+            i //= 2
+
+    def insert (self , key) :
+        self.heap_list.append (key)
+        self.current_size += 1
+        self.percUp (self.current_size)
+
+    def percDown (self , i) :
+        while (i * 2) <= self.current_size :
+            mc = self.maxChild (i)
+
+            if self.heap_list [i] < self.heap_list [mc] :
+                self.heap_list [i] , self.heap_list [mc] = self.heap_list [mc] , self.heap_list [i]
+
+            i = mc
+
+    def maxChild (self , i) :
+        if (i * 2) + 1 > self.current_size :
+            return i * 2
+
+        else :
+            if self.heap_list [i * 2] > self.heap_list [(i * 2) + 1] :
+                return i * 2
+
+            else :
+                return (i * 2) + 1
+
+    def delMax (self) :
+        the_max = self.heap_list [1]
+
+        self.heap_list [1] = self.heap_list.pop ()
+        self.current_size -= 1
+        self.percDown (1)
+
+        return the_max
+
+    def buildHeap (self , a_list) :
+        i = len (a_list) // 2
+
+        self.current_size = len (a_list)
+        self.heap_list = [0] + a_list [ : ]
+
+        while i > 0 :
+            self.percDown (i)
+
+            i -= 1
